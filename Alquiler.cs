4d336f1ed824d12f76a00b1fffb02856e30e8b6a
Marcelo David Barrientos Muñoz <marcebarr241@mesoamericana.edu.gt>
2021-03-08 00:31:38 -0600

    creacion de la clase para el alquiler de vehiculos

---
+++ Alquiler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehiculos
+{
+    class Alquiler
+    {
+        string nit;
+        string placa;
+        string fecha_alquiler;
+        string fecha_devolucion;
+        double k_recorridos;
+
+        public string Nit { get => nit; set => nit = value; }
+        public string Placa { get => placa; set => placa = value; }
+        public string Fecha_Alquiler { get => fecha_alquiler; set => fecha_alquiler = value; }
+        public string Fecha_Devolucion { get => fecha_devolucion; set => fecha_devolucion = value; }
+        public double K_Recorridos { get => k_recorridos; set => k_recorridos = value; }
+        
+        
+    }
+}
